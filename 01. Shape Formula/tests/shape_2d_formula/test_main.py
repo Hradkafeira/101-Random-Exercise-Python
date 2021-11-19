ed918b05cf92f12e0075a684658de7b4217d25d1
hradkafeira <ariefakbarhidayat1098@gmail.com>
2021-11-19 10:38:21 +0700

    fix indentation #1

--- 01. Shape Formula/tests/shape_2d_formula/test_main.py
+++ 01. Shape Formula/tests/shape_2d_formula/test_main.py
@@ -5,9 +5,9 @@ sd=sd()
 
 class TestSquare(object):
 	def test_square_normal(self):		
-			assert sd.square(2) == 4
+		assert sd.square(2) == 4
 	def test_square_normal2(self):
-			assert sd.square(1.5)==2.25
+		assert sd.square(1.5)==2.25
 
 class TestTriangle(object):
 	def test_triangle_normal(self):		
@@ -20,24 +20,24 @@ class TestTriangle(object):
 
 class TestRectangle(object):
 	def test_rectangle_normal(self):		
-			assert sd.rectangle(2,4) == 8
+		assert sd.rectangle(2,4) == 8
 	def test_rectangle_normal2(self):		
-			assert sd.rectangle(4,4) == 16
+		assert sd.rectangle(4,4) == 16
 
 class TestCircle(object):
 	def test_circle_normal(self):		
-			assert sd.circle(7) == 154
+		assert sd.circle(7) == 154
 	def test_circle_normal2(self):		
-			assert sd.circle(21) == 1386
+		assert sd.circle(21) == 1386
 
 class TestParrallelogram(object):
 	def test_parallelogram_normal(self):		
-			assert sd.parallelogram(20,10) == 200
+		assert sd.parallelogram(20,10) == 200
 	def test_parallelogram_normal2(self):		
-			assert sd.parallelogram(1,2) == 2
+		assert sd.parallelogram(1,2) == 2
 
 class TestTrapezium(object):
 	def test_trapezium_normal(self):		
-			assert sd.trapezium(2,3,2) == 5
+		assert sd.trapezium(2,3,2) == 5
 	def test_trapezium_normal2(self):		
-			assert sd.trapezium(4,4,3) == 12 
+		assert sd.trapezium(4,4,3) == 12 
